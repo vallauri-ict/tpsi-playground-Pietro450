e8def650867fefe0347870650f3bde00072e6d91
Pietro450 <71272907+Pietro450@users.noreply.github.com>
2021-01-18 21:59:22 +0100

    Update script.js

--- 003-RandomUser/script.js
+++ 003-RandomUser/script.js
@@ -1,171 +1,89 @@
-"use strict";
-let txtUtenti;
-let sliderUtenti;
-let chkFiltri;
-let val=0;
-let naz=["CH", "BR", "NL", "IT", "FI","US"];
-let genere=["MASCHIO","FEMMINA"];
-let password=["C. SPECIALI","MAIUSCOLA","MINUSCOLA","NUMERI"];
-let dati;
-let nGenerati;
-let cont=1;
-window.onload = function () {
-    nGenerati=document.getElementsByClassName("generati");
-    txtUtenti=document.getElementById("numeroUtenti");
-    sliderUtenti=document.getElementById("sliderUtenti");
-    chkFiltri=document.getElementsByClassName("checkBoxFiltri");
-    for(let i=0; i<chkFiltri.length; i++)
-    {
-        caricaCheck(chkFiltri[i].value,chkFiltri[i]);
-        cambiaStatoCheck(chkFiltri[i].value,chkFiltri[i])
-    }
-}
-function generateUsers() {
-    if(txtUtenti.value!="")
-    {
-    let numUsers=txtUtenti.value;
-    let param="?results="+numUsers+"&&"+aggiungiFiltri();
-    let finalUrl="https://randomuser.me/api"+param;
-    console.log(finalUrl);
-    //--------------------------------------------------------------
-    //--------------------------------------------------------------
+//il codice commentato è quello iniziale fatto solo in JavaScript
+//la secoda parte è quello implmentato in JQuery
+"use strict"
+let i =0;
+let nUser=0;
+let data=null;
+let gender;
+let nat;
+
+function genera() {
+    gender=$("#TendinaGender option:selected").text();
+    //gender=document.getElementById("TendinaGender").options[document.getElementById("TendinaGender").selectedIndex].innerText;
+    nat=$("#TendinaNationality option:selected").text();
+    //nat=document.getElementById("TendinaNationality").options[document.getElementById("TendinaNationality").selectedIndex].innerText;
+    nUser=$("#txtUtenti").val();
+     //nUser= document.getElementById("txtUtenti").value;
+    let parameters= '/?results=' + nUser + "&gender=" + gender + "&nat=" + nat + "&noinfo"; //con il no info non richiedo informazioni aggiuntive
+    let finalUrl='https://randomuser.me/api' + parameters;
+    //chiamata ajax
     $.ajax({
-        url:finalUrl,
+        url: finalUrl,
         dataType: 'json',
-        success:function (usersData) {
-            cont=1;
-            console.log(usersData);
-            dati=usersData.results;
-            caricaDati(0,dati);
-            document.getElementById("labelMax").innerText="di "+dati.length;
+        success: function (usersData) {
+            data = usersData;
+            CreaImg();
         }
-    })
-    }else{
-        alert("inserire numero");
-    }
-}
-function aggiungiFiltri() {
-    let filtri="";
-    for(let i=0; i<chkFiltri.length; i++){
-        if (chkFiltri[i].checked)
-        {
-            filtri+=chkFiltri[i].value+"=";
-            let array=document.getElementsByClassName(chkFiltri[i].value);
-            let primoFiltro=true;
-            for(let j=0;j<array.length;j++){
-                if(array[j].checked){
-                    let filtro="_"+array[j].id;
-                    if(primoFiltro)
-                        primoFiltro=false;
-                    else{
-                        filtri+=",";
-                    }
-                    filtri+=document.getElementById(filtro).innerText;
-                }
-            }
-            filtri+="&&";
-        }
-    }
-    return filtri;
-}
-//-----------------------------------------------------
-//-----------------------------------------------------
-function testoCambiato() {
-    console.log(sliderUtenti.max);
-    if (parseInt(txtUtenti.value)>sliderUtenti.max||(txtUtenti.value<sliderUtenti.min&&txtUtenti.value!=""))
-    {
-        txtUtenti.value=val;
-    }else{
-        val=txtUtenti.value;
-        sliderUtenti.value=val;
-    }
-}
-//--------------------------------------------------------
-//--------------------------------------------------------
-//carico utenti
-function caricaCheck(tipo, checkAbilitaFiltro)
-{
-    switch (tipo)
-    {
-        case "nat":
-            generaRadioOcheck(naz,"checkbox","nat",checkAbilitaFiltro.parentNode);
-            break;
-        case "gender":
-          generaRadioOcheck(genere,"radio","gender",checkAbilitaFiltro.parentNode);
-            break;
-        case "password":
-            generaRadioOcheck(password,"checkbox","password",checkAbilitaFiltro.parentNode);
-            break;
-    }
-}
-function generaRadioOcheck(array,tipo,name,doveInserire)
-{
-    for (let i=0;i<array.length;i++)
-    {
-        let labelRadioOCheck=document.createElement("label");
-        labelRadioOCheck.innerText=array[i];
-        labelRadioOCheck.for=array[i];
-        labelRadioOCheck.id="_"+array[i];
-        doveInserire.appendChild(labelRadioOCheck);
-        let radioOCheck=document.createElement("input");
-        radioOCheck.id=array[i];
-        radioOCheck.type=tipo;
-        radioOCheck.value=array[i];
-        radioOCheck.name=name;
-        radioOCheck.checked=false;
-        radioOCheck.setAttribute("class",name);
-        doveInserire.appendChild(radioOCheck);
-    }
-}
-//carico i dati
-//--------------------------------------------------------------------
-function caricaDati()
-{
-    let indice=cont-1;
-    pulisciDati();
-    document.getElementById("labelContatore").innerText="elemento "+cont;
-    nGenerati[0].src=dati[indice].picture.thumbnail;
-  nGenerati[1].innerText+=" "+ dati[indice].name.first;
-  nGenerati[2].innerText+=" "+dati[indice].name.last;
-  nGenerati[3].innerText+=" "+ dati[indice].gender;
-    nGenerati[4].innerText+=" "+ dati[indice].nat;
-  nGenerati[5].innerText+=" "+dati[indice].email;
-    nGenerati[6].innerText+=" "+dati[indice].login.password;
+    });
 }
 
-//----------------------------------------------------------------------
-function pulisciDati(){
- for(let i=0; i<nGenerati.length; i++)
- {
-     nGenerati[i].innerText=nGenerati[i].id+": ";
- }
-}
-//-----------------------------------------------------------------------------
-function cambiaStatoCheck(tipo,chkMaster) {
-    switch (tipo)
+function nationality() {
+    let nationality= "AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NO, NL, NZ, TR, US".split(',');
+    for (let i=0; i<nationality.length;i++)
     {
-        case "nat":
-            cambiaStato(naz,chkMaster.checked);
-            break;
-        case "gender":
-            cambiaStato(genere,chkMaster.checked);
-            break;
-        case "password":
-            cambiaStato(password,chkMaster.checked);
-            break;
+        //document.getElementById("TendinaNationality").innerHTML+="<option>" + nationality[i].trim() + "</option>";
+        $("#TendinaNationality").append("<option>" + nationality[i].trim() + "</option>");
     }
 }
-//scegliere naz
-function cambiaStato(array,valore) {
-    for (let i=0;i<array.length;i++){
-        let chkOradio=document.getElementById(array[i]);
-        chkOradio.disabled=!valore;
-    }
+
+//metodo per inserire l'immagine dell'utente generato
+function CreaImg() {
+    //let Img= document.getElementById("imgUtente");
+    //Img.setAttribute("src", data.results[i].picture.large);
+    $("#imgUtente").attr("src", data.results[i].picture.large);
+    //document.getElementById("1").innerText=data.results[i].name.last+" "+data.results[i].name.first;
+    $("#1").text(data.results[i].name.last+" "+data.results[i].name.first);
 }
-function cambiaIndice(step){
-    step=parseInt(step);
-    if((cont+step)>0&&(cont+step)<(dati.length+1)){
-    cont+=step;
- caricaDati();
+
+//funzione che mi permette di farevedere gli elementi richiesti alla pressione del bottone
+function Vedi(puntatore) {
+    //in questa funzione utilizzo tutti if perchè webstorm mi dava problemi con gli else if non facendo vedere gli elementi richiesti premendo il bottone
+    if (puntatore.id=="Nome")
+    {
+        $("#1").text(data.results[i].name.last + " " + data.results[i].name.first); //nome e cognome
+        //document.getElementById("1").innerText=data.results[i].name.last+" "+data.results[i].name.first;
     }
+    if (puntatore.id=="Indirizzo")
+    {
+        $("#1").text(data.results[i].location.street.number + " " + data.results[i].location.city); //indirizzo
+        //document.getElementById("1").innerText=data.results[i].location.street.number+" "+data.results[i].location.city;
+    }
+    if (puntatore.id=="Mail")
+    {
+        $("#1").text(data.results[i].email); //email
+        //document.getElementById("1").innerText=data.results[i].email;
+    }
+    if(puntatore.id=="Telefono")
+    {
+        $("#1").text(data.results[i].phone); //telefono
+        //document.getElementById("1").innerText=data.results[i].phone;
+    }
+    //else//document.getElementById("1").innerText=data.results[i].mappa;
+    // $("#1").text(data.results[i].mappa); //mappa
 }
+
+//bottone avanti
+function avanti() {
+    i++;
+    if (i==nUser)
+        i=0;
+    CreaImg();
+    }
+
+//bottone indietro
+function indietro() {
+    i--;
+    if (i==-1)
+        i=nUser-1;
+    CreaImg();
+    }
